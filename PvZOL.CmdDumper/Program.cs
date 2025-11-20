9e485c73c7f98c53d905b171b5555b07e2daee56
zingballyhoo <zingballyhoo@live.co.uk>
2025-11-20 20:55:47 +0000

    begin cmd dumper

---
+++ PvZOL.CmdDumper/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using PvZOL.CmdDumper;
+
+internal static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var inputFile = args[0];
+
+        var ffdecOutDir = Path.Combine("decompiled");
+        Directory.CreateDirectory(ffdecOutDir);
+        
+        await RunFFDec(inputFile, ffdecOutDir);
+        await GenerateFiles(ffdecOutDir);
+    }
+
+    private static async Task RunFFDec(string inputFile, string ffdecOutDir)
+    {
+        var proc = new Process()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = @"C:\Program Files (x86)\FFDec\ffdec-cli",
+                Arguments = $"-selectclass PVZ.++ -onerror abort -export script {ffdecOutDir} {inputFile}"
+            }
+        };
+        proc.Start();
+        await proc.WaitForExitAsync();
+        if (proc.ExitCode != 0)
+        {
+            throw new Exception("ffdec failed");
+        }
+    }
+    
+    private static async Task GenerateFiles(string ffdecOutDir)
+    {
+        var pvzDir = Path.Combine(ffdecOutDir, "scripts", "PVZ");
+
+        //var sourceFilePath = Path.Combine(pvzDir, "Cmd", "CmdCommon.as");
+        foreach (var sourceFilePath in Directory.EnumerateFiles(pvzDir, "*.as", SearchOption.AllDirectories))
+        {
+            var sourceFileName = Path.GetFileNameWithoutExtension(sourceFilePath);
+            if (sourceFileName == "CmdConst")
+            {
+                // nothing helpful
+                continue;
+            }
+    
+            var sourceFile = await File.ReadAllTextAsync(sourceFilePath);
+            if (sourceFile.Contains("extends Message"))
+            {
+                // todo: message id regex: `public static const \$MessageID:String = "([^"]*)";`
+                // needed for wire
+        
+                var pbType = new ProtoType(sourceFileName);
+                pbType.DecompileFields(sourceFile);
+                Console.Out.WriteLine(pbType.Emit());
+            }
+    
+            //throw new InvalidDataException("isn't a message");
+    
+            // todo: assume this is an enum..
+        }
+    }
+}
+
+
---
+++ PvZOL.CmdDumper/ProtoType.cs
@@ -0,0 +1,110 @@
+﻿using System.CodeDom.Compiler;
+using System.Text.RegularExpressions;
+
+namespace PvZOL.CmdDumper
+{
+    public record ProtoType(string m_typeName)
+    {
+        public List<ProtoField> m_fields = new List<ProtoField>();
+        
+        public void DecompileFields(string source)
+        {
+            var reader = new StringReader(source);
+            
+            while (reader.ReadLine() is { } line)
+            {
+                if (!line.Contains("public static const ") || !line.Contains("FieldDescriptor"))
+                {
+                    // not a field def
+                    continue;
+                }
+
+                var fieldRegex = new Regex(@"public static const [^ ]+ = new (Repeated)?FieldDescriptor\$TYPE_([^ (]+)\((.+)\);$");
+                var fieldMatch = fieldRegex.Match(line);
+                if (!fieldMatch.Success)
+                {
+                    throw new InvalidDataException($"unable to match line: \"{line}\"");
+                }
+
+                var repeatedSpecifier = fieldMatch.Groups[1].Value;
+                var dataType = fieldMatch.Groups[2].Value;
+                var ctorParams = fieldMatch.Groups[3].Value;
+
+                var ctorParamsList = ctorParams.Split(',');
+                var ctorParam_FullName = ctorParamsList[0];
+                var ctorParam_ShortName = ctorParamsList[1];
+                var ctorParam_Flags = ctorParamsList[2];
+                //var ctorParam_Dto = ctorParamsList[3]; // if present
+
+                var fieldNumRegex = new Regex(@"(\d+) << 3");
+                var fieldNumMatch = fieldNumRegex.Match(ctorParam_Flags);
+                if (!fieldNumMatch.Success)
+                {
+                    throw new InvalidDataException($"unable to match field number: {ctorParam_Flags}");
+                }
+    
+                //Console.Out.WriteLine($"{dataType} {ctorParams}");
+
+                var csType = dataType switch
+                {
+                    "BOOL" => "bool",
+                    "INT32" => "int",
+                    "UINT32" => "uint",
+                    "INT64" => "long",
+                    "UINT64" => "ulong",
+                    "FLOAT" => "float",
+                    "DOUBLE" => "double",
+                    "STRING" => "string",
+                    "BYTES" => "byte[]",
+                    "MESSAGE" => ctorParamsList[3],
+                    "ENUM" => ctorParamsList[3],
+                    _ => throw new NotImplementedException($"unknown as type: {dataType}")
+                };
+
+                var field = new ProtoField(
+                    ctorParam_ShortName.Trim('"'),
+                    int.Parse(fieldNumMatch.Groups[1].ValueSpan),
+                    csType,
+                    repeatedSpecifier.Length != 0);
+                m_fields.Add(field);
+                
+                Console.Out.WriteLine(field);
+            }
+        }
+
+        public string Emit()
+        {
+            var writer = new IndentedTextWriter(new StringWriter());
+            writer.WriteLine("namespace PvZOL.Cmd.Gen.Types;");
+            
+            writer.WriteLine("[ProtoContract]");
+            writer.WriteLine($"public class {m_typeName}");
+            writer.WriteLine("{");
+            writer.Indent++;
+
+            foreach (var field in m_fields)
+            {
+                var type = field.m_elementType;
+                if (field.m_isRepeated)
+                {
+                    type = $"List<{type}>";
+                }
+                
+                writer.Write($"[ProtoMember({field.m_number})] public {type} m_{field.m_name}");
+                if (field.m_isRepeated)
+                {
+                    writer.Write(" = new()");
+                }
+                
+                writer.WriteLine(";");
+            }
+
+            writer.Indent--;
+            writer.WriteLine("}");
+
+            return writer.InnerWriter.ToString()!;
+        }
+    }
+
+    public record ProtoField(string m_name, int m_number, string m_elementType, bool m_isRepeated);
+}
